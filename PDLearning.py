957c3020d3a076c5a4e7466209195a701c7988b1
devops0086 <59302455+devops0086@users.noreply.github.com>
2020-03-26 01:07:36 -0400

    More practice.

--- PDLearning.py
+++ PDLearning.py
@@ -4,5 +4,21 @@ s = pd.Series([1,3,5,np.nan,6,8])
 print(s)
 dates=pd.date_range('20130101', periods = 6)
 print(dates)
-df=pd.DataFrame(np.random.randn(6,4),index=dates,columns=list('EFGH'))
+df=pd.DataFrame(np.random.randn(6,4),index=dates,columns=list(["Apple","Banana","Chocolate","Car"]))
 print(df)
+
+#the next set of dataframe is made for strings that turn to columns
+dates=pd.date_range('20130101', periods = 6)
+print(dates)
+df=pd.DataFrame(np.random.randn(6,4),index=dates,columns=list("EFGH"))
+print(df)
+
+#turn dictionaries into a Series
+df2 = pd.DataFrame({"A":1.,
+        'B':pd.Timestamp('20200101'),
+        'C':pd.Series(np.random.randn(), index=list(range(4)), dtype='float32'),
+        'D':np.array([3]*4,dtype='int32'),
+        'E':pd.Categorical(["test","test2","test3","test4"]),
+        'F':"foo"})
+print(df2)
+print(df2.dtypes)
