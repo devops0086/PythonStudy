4164fde1f23c9b43406ba0cc8c228b9063bd707e
devops0086 <59302455+devops0086@users.noreply.github.com>
2020-03-28 03:19:36 -0400

    APR calculator for pay day loans

---
+++ APRCalc.py
@@ -0,0 +1,10 @@
+borrow=int(input("How much would you like to borrow (round to nearest $100)? "))
+interest_rate=float(input("What is the interest rate on the loan? "))
+admin_cost=int(input("What is the admin cost per $100: "))
+every_x_dollar=100
+print("Please note the admin cost is,","$",str(admin_cost)," per, $100.")
+fees=(borrow/every_x_dollar)*admin_cost
+interest_exp=borrow*interest_rate
+payback=14
+APR=((((fees+interest_exp)/borrow)/payback)*365)*100
+print(round(float(APR),2),str("%"))
