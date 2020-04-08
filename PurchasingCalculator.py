b1177c2537942dafda948ffd7d915df39ee88889
devops0086 <59302455+devops0086@users.noreply.github.com>
2020-04-08 02:41:26 -0400

    Improvements to purchasing decision making. Next step, interest.

--- PurchasingCalculator.py
+++ PurchasingCalculator.py
@@ -1,13 +1,18 @@
-cost = float(input("What is the cost? "))
+end = str("y")
 taxes = 1.13
-paybackterm = float(input("What is the payback term? "))
-totalcost = cost*taxes
-discountindollars=(cost-float(input("Is there a discount in dollars (if no, enter '0')? ")))*1.13
-discountinpercentages=(cost*(1-float(input("Is there a discount in percentages (if no, enter '0')? "))))*1.13
-
-if discountinpercentages > 0:
-    print("Total",str(discountinpercentages)," ","Monthly at,",str(paybackterm)," is equal to",str(discountinpercentages/paybackterm))
-else:
-    print("Total",str(discountindollars)," ","Months to pay back:",str(paybackterm),"; is equal to",str(discountindollars/paybackterm))
-
-input("press enter to close:")
+while end != str("n"):
+    cost = float(input("What is the cost? "))
+    paybackterm = float(input("What is the payback term? "))
+    totalcost = cost*taxes
+    discountindollars=(cost-float(input("Is there a discount in dollars (if no, enter '0')? ")))*taxes
+    discountinpercentages=(cost*(1-float(input("Is there a discount in percentages (if no, enter '0')? "))))*taxes
+    print(discountindollars)
+    print(discountinpercentages)
+    if discountinpercentages > 0 == True:
+        print("1st Total",str(discountinpercentages)," ","Monthly at,",str(paybackterm)," is equal to",str(discountinpercentages/paybackterm))
+    elif discountindollars > 0 == True:
+        print("2nd Total",str(discountindollars)," ","Months to pay back:",str(paybackterm),"; is equal to",str(discountindollars/paybackterm))
+    elif (discountindollars == discountinpercentages) == True:
+        print("3rd Total",str(discountindollars)," ","Months to pay back:",str(paybackterm),"; is equal to",str(discountindollars/paybackterm))
+    end = str(input("Continue? press 'y' to continue, press 'n' to end: "))
+    
